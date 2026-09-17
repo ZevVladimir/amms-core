9b28456cf775aee4d4ec58175552dc588a809e55
ZevVladimir <zevvladimir3002@gmail.com>
2026-09-17 10:05:20 -0700

    add in generalized functions for determining the number of bins to make pixels square when using RA/Dec

--- src/amms/core/analysis/sky.py
+++ src/amms/core/analysis/sky.py
@@ -15,7 +15,43 @@ import numpy as np
 from astropy import units as u
 from astropy.coordinates import ICRS, SkyCoord
 
-__all__ = ["radec_from_galactocentric"]
+__all__ = ["radec_from_galactocentric", "sky_aspect", "sky_bins"]
+
+
+def sky_aspect(lat_center: float) -> float:
+    """
+    Calculates the display/box aspect (y-scale / x_scale) that corrects RA-like circles that shrink by cos(lat) away from the equator
+
+    This allows for the bins to remain square
+
+    This can be passed to show_map(..., aspect=sky_aspect(dec_center)) and to sky_bins() to make a box/pixel represent angular size not the raw lon/lat degrees
+    """
+    return 1.0 / np.cos(np.radians(lat_center))
+
+
+def sky_bins(
+    lon_range: tuple[float, float],
+    lat_range: tuple[float, float],
+    pixel_deg: float,
+    *,
+    lat_center: float | None = None,
+) -> tuple[int, int]:
+    """
+    Calculates the bin counts (nx, ny) for projet_lonlat such that the pixels are kept square in their angular size not raw lon/lat degrees
+
+    pixel_deg is what sets the true angular size per pixel
+    lat_center defaults to the range's midpoint
+    """
+    lon0, lon1 = lon_range
+    lat0, lat1 = lat_range
+    if lat_center is None:
+        lat_center = (lat0 + lat1) / 2.0
+    aspect = sky_aspect(lat_center)
+
+    nx = round((lon1 - lon0) / (pixel_deg * aspect))
+    ny = round((lat1 - lat0) / pixel_deg)
+
+    return nx, ny
 
 
 def radec_from_galactocentric(
